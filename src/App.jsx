315c68a96ba004ec82e6bea82412e55628fb0c99
hajar-hh <hhoseini1999@gmail.com>
2024-10-05 18:45:42 +0330

    react----11111

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HelloWorld from "./HelloWorld";
+import Card from "./Card";
+
+function App(){
+  return <>
+  <HelloWorld />
+  <Card />
+  </>
+}
+
+export default App
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import React from "react";
+
+function HelloWorld() {
+  return <h1 style={{ color: "red", fontSize: "150px" }}>👻Hello world👻</h1>;
+}
+
+export default HelloWorld;
